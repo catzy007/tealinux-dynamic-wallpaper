336cfcd346637e7566e9b2149053c77ea08c02a7
Bagus Saputra <38147269+catzy007@users.noreply.github.com>
2019-04-30 02:41:01 +0700

    force to use base 10

--- realtimeapi.sh
+++ realtimeapi.sh
@@ -31,24 +31,24 @@ if [ "$(curl -s "http://api.aladhan.com/v1/timingsByCity?city=Semarang&country=I
 	#API FROM https://aladhan.com/prayer-times-api#GetTimingsByCity
 
 	#set time according to API
-	seed1=$(( $(( $(( $(($(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[2]}" | cut -d':' -f 2) )) - $(( $(($(echo "${apiseed[1]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[1]}" | cut -d':' -f 2) )) )) / 5 ))
-	seed2=$(( $(( $(( $(($(echo "${apiseed[3]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[3]}" | cut -d':' -f 2) )) - $(( $(($(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[2]}" | cut -d':' -f 2) )) )) / 5 ))
-	apitime[0]=$(( $(($(echo "${apiseed[0]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[0]}" | cut -d':' -f 2) ))
-	apitime[1]=$(( $(($(echo "${apiseed[1]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[1]}" | cut -d':' -f 2) ))
+	seed1=$(( $(( $(( $((10#$(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[2]}" | cut -d':' -f 2) )) - $(( $((10#$(echo "${apiseed[1]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[1]}" | cut -d':' -f 2) )) )) / 5 ))
+	seed2=$(( $(( $(( $((10#$(echo "${apiseed[3]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[3]}" | cut -d':' -f 2) )) - $(( $((10#$(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[2]}" | cut -d':' -f 2) )) )) / 5 ))
+	apitime[0]=$(( $((10#$(echo "${apiseed[0]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[0]}" | cut -d':' -f 2) ))
+	apitime[1]=$(( $((10#$(echo "${apiseed[1]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[1]}" | cut -d':' -f 2) ))
 	apitime[2]=$(( ${apitime[1]} + seed1 ))
 	apitime[3]=$(( ${apitime[2]} + seed1 ))
 	apitime[4]=$(( ${apitime[3]} + seed1 ))
 	apitime[5]=$(( ${apitime[4]} + seed1 ))
-	apitime[6]=$(( $(($(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[2]}" | cut -d':' -f 2) ))
+	apitime[6]=$(( $((10#$(echo "${apiseed[2]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[2]}" | cut -d':' -f 2) ))
 	apitime[7]=$(( ${apitime[6]} + seed2 ))
 	apitime[8]=$(( ${apitime[7]} + seed2 ))
 	apitime[9]=$(( ${apitime[8]} + seed2 ))
 	apitime[10]=$(( ${apitime[9]} + seed2 ))
-	apitime[11]=$(( $(($(echo "${apiseed[3]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[3]}" | cut -d':' -f 2) ))
-	apitime[12]=$(( $(($(echo "${apiseed[4]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[4]}" | cut -d':' -f 2) ))
-	apitime[13]=$(( $(($(echo "${apiseed[5]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[5]}" | cut -d':' -f 2) ))
-	apitime[14]=$(( $(($(echo "${apiseed[6]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[6]}" | cut -d':' -f 2) ))
-	apitime[15]=$(( $(($(echo "${apiseed[7]}" | cut -d':' -f 1)*60)) + $(echo "${apiseed[7]}" | cut -d':' -f 2) ))
+	apitime[11]=$(( $((10#$(echo "${apiseed[3]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[3]}" | cut -d':' -f 2) ))
+	apitime[12]=$(( $((10#$(echo "${apiseed[4]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[4]}" | cut -d':' -f 2) ))
+	apitime[13]=$(( $((10#$(echo "${apiseed[5]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[5]}" | cut -d':' -f 2) ))
+	apitime[14]=$(( $((10#$(echo "${apiseed[6]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[6]}" | cut -d':' -f 2) ))
+	apitime[15]=$(( $((10#$(echo "${apiseed[7]}" | cut -d':' -f 1)*60)) + 10#$(echo "${apiseed[7]}" | cut -d':' -f 2) ))
 
 	for index in ${!apitime[@]}; do
 		if [ "${index}" == "0" ]; then
